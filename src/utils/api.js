8cc021bd25641d2e047744a7c637f8abde021cc5
Dhruvish <php.dhruvish@spaculus.info>
2026-03-11 17:15:09 +0530

    Fix API URL call

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   headers: {
+//     // Let axios automatically set Content-Type, especially for FormData
+//   },
+// });
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     // Let axios automatically set Content-Type, especially for FormData
   },
